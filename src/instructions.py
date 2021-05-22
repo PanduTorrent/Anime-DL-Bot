5136e52ae8cde560882120f0124776cc3e8755d3
PanduTorrent <80548157+PanduTorrent@users.noreply.github.com>
2021-05-22 11:11:28 +0530

    Update instructions.py

--- src/instructions.py
+++ src/instructions.py
@@ -15,11 +15,9 @@ def instructions(client, callback_query):
         [InlineKeyboardButton("Search Anime Inline", switch_inline_query_current_chat="")]
     ]
     reply_markup = InlineKeyboardMarkup(keyb)
-    query.edit_message_caption(caption="""**This Bot can Get your favourite Anime and It provides FREE Download Link with a fastest server(Google drive). ❤️😍**
+    query.edit_message_caption(caption="""**This Bot can Get your favourite Anime and It provides Download Link with a fastest server(Google drive).**
 
 **Points to Be Noted :-**
-
-__👉Since gogoanime changes their domain often, The bot will go for frequent maintenance. Don't worry, the bot will still be online during maintenance.__
 
 __👉For streaming in mobile, open the links with VLC Media Player. You can also use MX Player.__
 
@@ -27,6 +25,8 @@ __👉For streaming in PC, use VLC media player network stream.__
 
 __👉For downloads, just open the links in a browser.__
 
-**That's it, You are all caught up, just start and enjoy your favourite otaku anime😁😆**
+**That's it, ENJOY BITCHES 😁✌**
+
+**BOT MADE BY @pie_yush. For personal use only. If source used without permission, INSTANT BAN**
 
 **Type /search to Search for an Anime...**""", parse_mode="markdown", reply_markup=reply_markup)
